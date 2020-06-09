fcd8bd30b31607c0ee4224593d858471171d47aa
Julien Bouland <bouland.julien@gmail.com>
2020-06-09 20:40:35 +0200

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "page#home"
+  get "/profile", to: 'pages#profile'
+  ressources :gardens #, only: [:create, :show, :destroy]
+  ressources :plants #, only: [:create, :destroy]
 end
