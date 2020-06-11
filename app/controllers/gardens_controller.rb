bf5fe0b5f6f6fbae47fab35757b2ed60571496ae
Julien Bouland <bouland.julien@gmail.com>
2020-06-11 19:48:53 +0200

    garden controller implemented

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -3,7 +3,32 @@ class GardensController < ApplicationController
     @garden = Garden.all
   end
 
-  def method_name
+  def show
+    @garden = Garden.find(params[:id])
+  end
 
+  def new
+    @garden = Garden.new
+  end
+
+  def create
+    @garden = Garden.new(garden_params)
+    @garden.user = current_user
+    if @garden.save
+      redirect_to garden_path(@garden)
+      flash[:success] = 'Bienvenue à bord ! Vous pouvez maintenant ajouter des plantes à votre jardin 🌱'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+private
+
+  def garden_params
+    params.require(:garden).permit(:name, :plant_id)
   end
 end
