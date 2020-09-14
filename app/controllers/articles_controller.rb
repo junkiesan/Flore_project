f8582413f916e1a1308f39c92f2c83bce13198c7
Julien Bouland <bouland.julien@gmail.com>
2020-09-14 18:19:34 +0200

    Permit bug removed frome Article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,6 +45,6 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit!(:title, :body, :rich_body, photos: [])
+    params.require(:article).permit(:title, :rich_body, photos: [])
   end
 end
