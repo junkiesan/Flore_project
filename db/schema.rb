3bb8117c2b7aea097db00a100c699c565d5e9d7b
Julien Bouland <bouland.julien@gmail.com>
2020-09-14 18:10:58 +0200

    Add body to Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_145650) do
+ActiveRecord::Schema.define(version: 2020_09_14_160855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_145650) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body", null: false
   end
 
   create_table "gardens", force: :cascade do |t|
